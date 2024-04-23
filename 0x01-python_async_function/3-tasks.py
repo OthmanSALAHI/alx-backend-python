68c2fd0c614b0afa43aafea5d1d6c48363f77bb8
Othmansalahi <ootmansalhi.24@gmail.com>
2024-04-23 10:53:57 +0100

    Refactor task_wait_n function to use asyncio.create_task in 0x01-python_async_function/3-tasks.py

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python3
 """ Module documentation """
 import asyncio
-import random
-from typing import List
+import time
 
-task_wait_random = __import__("3-tasks").task_wait_random
+wait_random = __import__("0-basic_async_syntax").wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> List[float]:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """doc func"""
-    listDelays = []
-    for _ in range(n):
-        listDelays.append(task_wait_random(max_delay))
-    return sorted(await asyncio.gather(*listDelays))
+    return asyncio.create_task(wait_random(max_delay))
