327139df2f8e363ff5f67f8a47d9ffeb3b4b11c9
Othmansalahi <ootmansalhi.24@gmail.com>
2024-04-23 10:53:53 +0100

    Refactor measure_time function to include type annotations in 0x01-python_async_function/2-measure_runtime.py

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -6,7 +6,7 @@ import time
 wait_n = __import__("1-concurrent_coroutines").wait_n
 
 
-def measure_time(n, max_delay):
+def measure_time(n: int, max_delay: int) -> float:
     """doc func"""
     start = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
